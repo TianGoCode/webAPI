146bd5c0f3e656f1d98c1ce25a765203935cae37
TianGoCode <tuan.vuhoang.bk@gmail.com>
2020-12-12 12:28:10 +0700

    api sign up, thieu 1 kiem tra ky tu

--- routes/web.php
+++ routes/web.php
@@ -30,66 +30,83 @@ Route::get('/home', function () {
     return view('home');
 });
 
-Route::get('/signup', function (){
+Route::get('/signup', function () {
     return view('signUp');
 });
 
-Route::post('/signup',function(Request $request){
-   $user = new User();
-   $user->phone = $request->input('phone');
-   $user->pass = bcrypt($request->input('pass'));
-   $user->uuid = Uuid::generate()->string;
-   
+Route::post('/signup', function (Request $request) {
+    // lay ra thong tin request duoc gui len
+    $user = new User();
+    $user->phonenumber = $request->input('phone');
+    $user->password = bcrypt($request->input('pass'));
+//    $user->uuid = $request->input('uuid');
+    
+    $duplicate = User::where('phonenumber', $user->phonenumber)->first();
+
+    if ($duplicate) {
+
+        //kiem tra trung lap sdt
+        return response()->json([
+            "code" => 9996,
+            "message" => "sdt ban dang ky da trung",
+            "data" => $request->all()
+        ]);
+    }
+    if ($request->input('phone') == null) {
+        //neu chua nhap sdt
+        return response()->json([
+            "code" => "error",
+            "message" => "ban chua nhap so dien thoai",
+            "data" => $request->all()
+        ]);
+    }
+    if ($request->input('pass') == null) {
+        // neu chua nhap mk
+        return response()->json([
+            "code" => "error",
+            "message" => "ban chua nhap mat khau",
+            "data" => $request->all()
+        ]);
+    }
+    if ($request->input('phone') == $request->input('pass')) {
+        //neu mk trung sdt
+        return response()->json([
+            "code" => "error",
+            "message" => "ban da nhap sodien thoai trung mat khau",
+            "data" => $request->all()
+        ]);
+    }
+
     $user->save();
 
-   return response()->json([
-       "user vua dang ky"=>$user,
-       "thong tin vua nhan"=>$request->all()
+    return response()->json([
+        "code" => 1000,
+        "message" => "dang ky thanh cong",
+        "data" => $request->all()
+    ]);
 
-
-
-
-
-       //2 nhiem vu cua dang ky : luu nguoi dung va testcase : test case dung het thi moi luu nguoi dung !
-   ]);
 });
 
-//Route::post('/signup',function (Request $request){
-//    return response()->json([
-//        'code'=>'1000',
-//        'response'=>$request->all()
-//    ]);
-//});
 
-
-//Route::post('/home',function (Request $request){
-////    return view('login')->with(['request'=>$request->all()]);
-//    return response()->json([
-//        'code'=>'1000',
-//        'response'=>$request->all()
-//    ]);
-//});
-
-Route::get('/login',function() {
+Route::get('/login', function () {
     return view('logIn');
 });
 
-Route::post('/login', function(Request $request) {
+Route::post('/login', function (Request $request) {
 
     $data = [
-            'phone'=> $request->phoneIn,
-            'pass' => $request->passIn
+        'phone' => $request->phoneIn,
+        'pass' => $request->passIn
     ];
-   
+
     $request->validate([
-            'phoneIn' => 'required|min:4',
-            'passIn' => 'required'
+        'phoneIn' => 'required|min:4',
+        'passIn' => 'required'
     ]);
 
-         if(Auth::attempt($data)){
-            dd('Đăng nhập thành công');
-        }
-         else {
-            dd('TK hoặc MK chưa đúng');
-        }
+    if (Auth::attempt($data)) {
+        dd('Đăng nhập thành công');
+    } else {
+        dd('TK hoặc MK chưa đúng');
+    }
 });
