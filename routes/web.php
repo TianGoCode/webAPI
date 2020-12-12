c50d526abd1e97a253244c9f3580f56e4fd1a50c
vuyycc <vu.ttrandoan@gmail.com>
2020-12-12 20:15:30 +0700

    them sai dinh dang sdt

--- routes/web.php
+++ routes/web.php
@@ -35,95 +35,33 @@ Route::get('/signup', function () {
 });
 
 Route::post('/signup', function (Request $request) {
-    //test
-    // $rules = [
-    //     'phone' => 'required|numeric|phone_number|size:10',
-    //     'pass' => 'required|min:6|max:8'
-    // ];
+  
 
-    // $messages = [
-    //     'phone.required' => 'Sdt khong trong',
-    //     'phone.numeric' => 'Phai la so',
-    //     'phone.phone_number' => 'Bat dau bang so 0',
-    //     'phone.size' => 'Phai 10 so',
-    //     'pass.required' => 'Pass khong trong',
-    //     'pass.min' => 'Khong nho hon 6',
-    //     'pass.max' => 'Khong dc lon hon 8',
-    // ];
-
-    // $validator = Validator::make($request -> all(), $rules, $messages);
-
-    // return response()->json([
-    //     'mess' => $messages,
-    //     'rules' => $rules,
-    //     'validate' => $validator->fails()
-    // ]);
-
-    // if($validator -> fails()){
-    //     return redirect('signup')->withErrors($validator)->withInput();
-    // };
-
-
-    // lay ra thong tin request duoc gui len
-    // $user = new User();
-    // $user->phonenumber = $request->input('phone');
-    // $user->password = bcrypt($request->input('pass'));
+//    lay ra thong tin request duoc gui len
+    $user = new User();
+    $user->phonenumber = $request->input('phone');
+    $user->password = bcrypt($request->input('pass'));
 //    $user->uuid = $request->input('uuid');
-<<<<<<< HEAD
-    
-    // $duplicate = User::where('phonenumber', $user->phonenumber)->first();
-
-    // if ($duplicate) {
-
-    //     //kiem tra trung lap sdt
-    //     return response()->json([
-    //         "code" => 9996,
-    //         "message" => "sdt ban dang ky da trung",
-    //         "data" => $request->all()
-    //     ]);
-    // }
-    // if ($request->input('phone') == null) {
-    //     //neu chua nhap sdt
-    //     return response()->json([
-    //         "code" => "error",
-    //         "message" => "ban chua nhap so dien thoai",
-    //         "data" => $request->all()
-    //     ]);
-    // }
-    // if ($request->input('pass') == null) {
-    //     // neu chua nhap mk
-    //     return response()->json([
-    //         "code" => "error",
-    //         "message" => "ban chua nhap mat khau",
-    //         "data" => $request->all()
-    //     ]);
-    // }
-    // if ($request->input('phone') == $request->input('pass')) {
-    //     //neu mk trung sdt
-    //     return response()->json([
-    //         "code" => "error",
-    //         "message" => "ban da nhap sodien thoai trung mat khau",
-    //         "data" => $request->all()
-    //     ]);
-    // }
-
-    // $user->save();
-
-    // return response()->json([
-    //     "code" => 1000,
-    //     "message" => "dang ky thanh cong",
-    //     "data" => $request->all()
-    // ]);
-=======
 
     $duplicate = User::where('phonenumber', $user->phonenumber)->first();
+
+
+    $phoneNumber = $request->input('phone');
+    $oneNum = substr($phoneNumber,0,1);
+    if(strlen($phoneNumber) != 10 || $oneNum != '0'){
+        return response()->json([
+            "code" =>1004,
+            "message" => "Sai định dạng số điện thoại",
+            "data" => $request -> all()
+        ]);
+    }
 
     if ($duplicate) {
 
         //kiem tra trung lap sdt
         return response()->json([
             "code" => 9996,
-            "message" => "sdt ban dang ky da trung",
+            "message" => "Người dùng đã tồn tại",
             "data" => $request->all()
         ]);
     }
@@ -160,7 +98,6 @@ Route::post('/signup', function (Request $request) {
         "message" => "dang ky thanh cong",
         "data" => $request->all()
     ]);
->>>>>>> 80ea4364a63bff1344a354938aeffc132256b8eb
 
 });
 
